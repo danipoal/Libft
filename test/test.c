61c27aebcfeef4a195235c06d2a2ba2c5facc5df
danipoal <danipoal.0@gmail.com>
2025-01-15 13:50:16 +0100

    Test update

--- test/test.c
+++ test/test.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: danalvar <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/15 12:49:13 by danalvar          #+#    #+#             */
+/*   Updated: 2025/01/15 13:18:52 by danalvar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../libft.h"
 #include <stdio.h>
 #include <ctype.h>
@@ -9,7 +21,7 @@
  */
 int	check(int i1, int i2)
 {
-	return (i1 == i2) || (i1 > 0 && i2 > 0);
+	return ((i1 == i2) || (i1 > 0 && i2 > 0));
 }
 
 int	main(void)
@@ -22,6 +34,12 @@ int	main(void)
 	{
 		if (!check(isalpha(i), ft_isalpha(i)))
 			printf("ft_isalpha failed in ascii %d\n", i);
+		if (!check(isdigit(i), ft_isdigit(i)))
+			printf("ft_isdigit failed in ascii %d\n", i);
+		if (!check(isalnum(i), ft_isalnum(i)))
+			printf("ft_isalnum failed in ascii %d\n", i);
+		if (!check(isascii(i), ft_isascii(i)))
+			printf("ft_isascii failed in ascii %d\n", i);
 		i++;
 	}
 	return (0);
