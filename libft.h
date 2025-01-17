bfd86f5003347aedbe0a72bc10d9a65eef268672
danipoal <danipoal.0@gmail.com>
2025-01-17 15:46:42 +0100

    SetUp memset to be tested

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: danalvar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/08 18:52:59 by danalvar          #+#    #+#             */
-/*   Updated: 2025/01/17 13:16:53 by danalvar         ###   ########.fr       */
+/*   Updated: 2025/01/17 13:57:31 by danalvar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,7 @@ char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 void	*ft_memchr(const void *s, int c, size_t n);
 void	*ft_memset(void *s, int c, size_t n);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	ft_bzero(void *s, size_t n);
 
 #endif
--- test/test.c
+++ test/test.c
@@ -6,7 +6,7 @@
 /*   By: danalvar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/15 12:49:13 by danalvar          #+#    #+#             */
-/*   Updated: 2025/01/17 12:25:59 by danalvar         ###   ########.fr       */
+/*   Updated: 2025/01/17 15:45:30 by danalvar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -142,35 +142,63 @@ void	atoi_tests()
 		printf("Err atoi for %s orig: %i ft: %i\n", str, atoi(str), ft_atoi(str));
 }
 
-/*
-void	memset_unit_test(void * s, int c)
-{
-	// Comparar dos *stacks de memoria
-	
 
+// Comparar dos *stacks de memoria
+void	memset_unit_test(void * s, int c, size_t n)
+{
+	int	i;
+	char	j;
+	void	*s2 = &i;
+	void	*s3 = &j;
+
+	i = 32;			// Iniciamos i a algo aleatorio
+	s2 = memcpy(s2, s, n);  // Le copiamos a i lo que viene en s para que sea lo mismo
+	j = 'a';
+	s3 = memcpy(s3, s, n);
+
+	// Comprobar valores antes de ser modificados
+	printf("Memset orig:%d ft: %d\n", *(int *) s, *(int *) s2);
+	printf("Memset orig:%c ft: %c\n", *(char *) s, *(char *) s2);
+
+	// TODO Para hacer la comparacion de memoria tiene que usarse memcmp, ya que al no
+	// 	Terminar con un '\0' hay que saber cuantos bytes comparar, y hacerlo en un
+	//	parseo apto n veces. 
+	if (*ft_memset(s2, c, n) != *memset(s, c, n))
+		printf("Error memset for int\n");
+	else
+		printf("Memset correct for int\n");
+	// Comprobar valores despues de ser modificados
+	printf("Memset orig:%d ft: %d\n", *(int *) s, *(int *) s2);
+	printf("Memset orig:%c ft: %c\n", *(char *) s, *(char *) s2);
+	
+	if (*ft_memset(s3, c, n) != *s)
+		printf("Error memset for char\n");
+	else
+		printf("Memset correct for char\n");
+
+	printf("Memset orig:%c ft: %c\n", *(char *) s, *(char *) s3);
 }
-*/
+
 
 /*
  * Tests for memchr(), memset()
  */
 
-/*
 void	mem_tests(void)
 {
+	// memset test
+	char s1[] = "Hola"; // No es aconsejable usar strings literales, por eso guardamos.
+	memset_unit_test(s1, 'x', 5); // Llenar de 0 la memoria que esta ocupada con "hola\0"
 	//memset_unit_test();
-
-
-
 }
-*/
+
 
 int	main(void)
 {
 	char_tests();
 	strlen_test();
 	str_tests();
-	//mem_tests();
+	mem_tests();
 	atoi_tests();
 	return (0);
 }
