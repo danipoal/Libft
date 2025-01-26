f356aac0519d1d80ded90e9ee663cd9dad25c998
danipoal <danipoal.0@gmail.com>
2025-01-26 17:15:07 +0100

    Checkpoint changing behaviour of str trim

--- src/ft_strtrim.c
+++ src/ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: danalvar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/26 15:52:53 by danalvar          #+#    #+#             */
-/*   Updated: 2025/01/26 16:41:29 by danalvar         ###   ########.fr       */
+/*   Updated: 2025/01/26 17:13:52 by danalvar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,55 @@
  *
  * @retuns The resultant string
  */
+
+
+/*
+ * Checks if the letter is equal to all characters in the set
+ *
+ */
+
+int	is_letter_set(char let, const char *set)
+{
+	int	i;
+
+	i = 0;
+	while (set[i])
+	{
+		if (let == set[i])
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+/*
+ * Checks the set for all the string, in the begining and in the final
+ *
+ * @param - direction: 0 -> Start to end. 	1 -> End to Start
+ * @returns The quantity of the letters suposed to be removed
+ */
+int	check_length(const char *s1, char const *set, int direction)
+{
+	int	isset;
+	int	i;
+	int	length_trimed;
+
+	length_trimed = 0;
+	isset = 1;
+	if (direction == 0)
+		i = 0;
+	else if (direction == 1)
+		i = ft_strlen();
+	while (s1[i] && isset)
+	{
+		if (is_letter_set(s1[i], set))
+			length_trimed ++;
+		else
+			isset = 0;
+		i++;
+	}
+	return (length_trimed);
+}
 
 int	ft_strtrim(const char *s1, char const *set)
 {
@@ -38,44 +87,9 @@ int	ft_strtrim(const char *s1, char const *set)
 	end = len - 1;
 
 	// Revisar string y encontrar
-	i = 0;
-	isset = 1;
-	// Principio
-	while (s1[i] && isset)
-	{
-		j = 0;
-		while (set[j])
-		{
-			if (s1[i] == set[j])
-			{
-				len--;
-				break;
-			}
-			j++;
-		}
-		if (j == ft_strlen(set))
-			isset = 0;
-		i++;
-	}
+	len = len - check_length(s1, set, 0);
 	// Final
-	isset = 1;
-	i = 0;
-	while (end > 0 && isset)
-	{
-		j = 0;
-		while (set[j])
-		{
-			if (s1[end] == set[j])
-			{
-				len--;
-				break;
-			}
-			j++;
-		}
-		if (j == ft_strlen(set))
-			isset = 0;
-		end --;
-	}
+	len = len - check_length(s1, set, 1)
 	return (len);
 
 	// Copiar en caso de que aun no se haya encontrado un char diferente
